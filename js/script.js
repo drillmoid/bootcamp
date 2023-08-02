ae50e552cde3d34162302345a3af5945dc9d8e13
drillmoid <drillmoid@gmail.com>
2023-08-02 11:58:16 +0300

    update script.js - special types

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ const number = 9;
 const text = 'This is some info'
 const bool = true;
 
+// types
+const array = []; 
+const object = {};
+
+// special types
+const undefinedvariable;
+console.log(undefinedvariable); // prints out undefined which implies that the variable has no value assigned 
+const emptyvariable = null; // prints out null meaning that the variable is empty 
+
