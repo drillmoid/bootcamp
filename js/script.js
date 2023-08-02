cff982989a2acc9eb28422546caf4cc57cd94c7a
drillmoid <drillmoid@gmail.com>
2023-08-02 15:14:36 +0300

    add arrays - arrays, addressing & elements

--- js/script.js
+++ js/script.js
@@ -11,8 +11,17 @@ const array = [];
 const object = {};
 
 // special types
-const undefinedvariable;
-console.log(undefinedvariable); // prints out undefined which implies that the variable has no value assigned 
+// [#] const undefinedvariable;
+// [#] console.log(undefinedvariable); // prints out undefined which implies that the variable has no value assigned 
 const emptyvariable = null; // prints out null meaning that the variable is empty 
 
+// arrays
+var frontend = ['js','html','css'];
+var backend = new Array('python','js');
+
+// addressing - query arrays by indices 
+console.log(frontend[1]); // expect html 
+
+// array elements
+var combination = ['text',0,[],{},true]
 
