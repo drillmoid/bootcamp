4fa20dd4497b30d12ccecb0cc00bac3319c0093f
drillmoid <drillmoid@gmail.com>
2023-08-02 16:10:01 +0300

    update script.js - manupulating arrays

--- js/script.js
+++ js/script.js
@@ -23,5 +23,37 @@ var backend = new Array('python','js');
 console.log(frontend[1]); // expect html 
 
 // array elements
-var combination = ['text',0,[],{},true]
+var combination = ['text',0,[],{},true];
+
+// manipulating arrays
+// pushing & popping - push > append at the end of an array & pop > drop at the end of an array
+var languages = [];
+
+// push
+languages.push('JavaScript');
+languages.push('HTML');
+languages.push('CSS');
+languages.push('Python');
+languages.push('C');
+languages.push('More Languages');
+
+// pop 
+languages.pop();
+
+// shifting and unshifting
+// shift and unshift are similar to the push and pop method. The only difference is that they work from the beginning of the array opposite to the push and pop - shift > pop unshift > push 
+
+// unshift
+languages.unshift('Java');
+languages.unshift('Kotlin');
+languages.unshift('Ruby');
+languages.unshift('Dart');
+languages.unshift('More Languages');
+
+// shift
+languages.shift();
+
+// splicing - removes a part of an array and creates a new array based on the part that we remove
+
+var android_languages = languages.splice(0,4);
 
