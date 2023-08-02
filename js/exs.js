b62f1ca4a02d7941d2c56bcc8872baf96c5921a2
drillmoid <drillmoid@gmail.com>
2023-08-02 14:51:37 +0300

    update exs.js file - variables and types exercise

---
+++ js/exs.js
@@ -0,0 +1,11 @@
+// ex 1 - variables and types 
+// q1 -You must define the following variables: (a) - A number called myNumber which contains the number 4; (b) - A string called myString which contains the sentence Variables are great.; (c) - A boolean called myBoolean which contains the value false;
+
+// q1 - a
+const myNumber = 4;
+
+// q1 - b
+const myString = 'Variables are great.';
+
+// q1 - c 
+const myBoolean = false;
