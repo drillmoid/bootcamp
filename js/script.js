1301cf5995c9e2c249cc71f32194260f7a0866ae
drillmoid <drillmoid@gmail.com>
2023-08-03 07:55:17 +0300

    update script.js - conditions

--- js/script.js
+++ js/script.js
@@ -94,3 +94,56 @@ console.log(age);
 // assignmet extends to - (*,-,/,%)  > *=,/=,-=,%=
 
 // more mathematics - js has a math module for advanced functions > abs - absolute value of a number, pow - powers of numbers x & y , etc
+
+// conditions
+// the if statement - allows us to check if an expression is true or fasle and excecute a piece of code according to the result
+var car = 'Audi';
+if (car == 'Audi')
+{
+  console.log('Perfect!')
+} else {
+  console.log('Only Audi!')
+}
+
+// equality evaluation operators
+// use (==) - check whether the compared are equal & (===) - check whether the compared are precisely equal in terms of type and value 
+
+// multiple expression evaluation
+// the and operator (&&) - check whether both compared are true , the or operator (||) - check whether atleast one of the compared is true & the not operator (!) - check the opposite boolean ie (!true == false)
+var student1Marks = 70;
+var student2Marks = 65;
+
+if (student1Marks && student2Marks > 60)
+{
+  console.log('Students passed!')
+} else {
+  console.log('Students failed!')
+}
+
+if (student1Marks || student2Marks > 69)
+{
+  console.log('Some student scored over 69!')
+} else {
+  console.log('No student scored over 69!')
+}
+
+var passStudent1 = true;
+
+if (!passStudent1)
+{
+  console.log('Student 1 Failed!')
+} else {
+  console.log('Student 1 Passed!')
+}
+
+// the  switch statement 
+// used to select between more than two different options and run the same code for more than one option
+var privilege = 'root';
+switch(privilege)
+{
+  case 'user1':
+    console.log('Superuser privilege denied!');
+    break;
+}
+
+
